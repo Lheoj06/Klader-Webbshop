1b1f355cfda534bbd75d36d434fff3de8fcfa3a3
Lheoj06 <lheo636@gmail.com>
2026-05-30 13:41:42 +0200

    Produktsidan hämtar nu produkter från db.json
    Kan nu se produkterna från databasen  på produkter sidan

--- client/src/Pages/Produkter.jsx
+++ client/src/Pages/Produkter.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 function Produkter() {
-  return <h1>Produkter</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Produkter</h1>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.price} kr</p>
+          <p>{product.category}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Produkter;
